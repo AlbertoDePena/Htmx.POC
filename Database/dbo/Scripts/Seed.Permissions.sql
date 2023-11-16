4c37b3cab8896d8207220524cf43084c1ef1adb3
Alberto De Pena <alberto.depena@craneww.com>
2023-11-16 08:28:23 -0600

    Oops

--- Database/dbo/Scripts/Seed.Permissions.sql
+++ Database/dbo/Scripts/Seed.Permissions.sql
@@ -1,6 +1,6 @@
 ﻿MERGE dbo.[Permissions] AS TARGET
 USING (Values
-('5417B133-69B4-436A-8B77-BFDAB49AAFD6','View Tranportation Data'),
+('5417B133-69B4-436A-8B77-BFDAB49AAFD6','View Transportation Data'),
 ('386B4CEC-0A92-4BB8-A39A-42B4B53FAB77','View Financials'),
 ('6C10AD06-67A8-4996-9DDA-CC253F9465F3','Export Search Results'))
 AS Source([Id],[Name])
