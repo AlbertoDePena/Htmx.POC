168292add8f234122d76e6a1707bb65b978cc3cf
Alberto De Pena <alberto.depena@craneww.com>
2023-11-30 14:11:13 -0600

    Refactoring

--- WebApplication/Domain/Extensions.fs
+++ WebApplication/Domain/Extensions.fs
@@ -7,10 +7,8 @@ module String =
     /// The default value of a string is null.
     let defaultValue = null
 
-    let ofEmailAddress (value: string) =
-        if isNull value then
-            None
-        elif Regex.IsMatch(value, @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$") |> not then
-            None
-        else
-            Some(value.ToLower())
+    let ofEmailAddress (value: string) =        
+        value
+        |> Option.ofObj
+        |> Option.filter (fun text -> Regex.IsMatch(text, @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$"))
+        |> Option.map (fun text -> text.ToLower())
