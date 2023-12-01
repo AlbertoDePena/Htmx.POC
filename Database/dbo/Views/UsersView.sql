159e6e2e00c71854edb7947ff7e137b26bebf715
Alberto De Pena <alberto.depena@craneww.com>
2023-11-30 20:23:33 -0600

    Added users view

---
+++ Database/dbo/Views/UsersView.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW [dbo].[UsersView]
+AS
+SELECT 
+	[Users].[Id],
+	[Users].[EmailAddress],
+	[Users].[DisplayName],
+	[Users].[TypeId],
+	[UserTypes].[Name] AS [TypeName],
+	[Users].[IsActive]
+FROM [dbo].[Users]
+LEFT JOIN [dbo].[UserTypes]
+	ON [Users].[TypeId] = [UserTypes].[Id];
