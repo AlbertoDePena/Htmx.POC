52c0d8cacd6a77e9334d61f3c8c55d5eba18901e
Alberto De Pena <alberto.depena@craneww.com>
2023-12-15 15:48:05 -0600

    Added serilog

--- WebApplication/Infrastructure/Telemetry.fs
+++ WebApplication/Infrastructure/Telemetry.fs
@@ -3,31 +3,83 @@
 open Microsoft.ApplicationInsights.Extensibility
 open Microsoft
 open Microsoft.AspNetCore.Http
+
+open System.Diagnostics
 open System.Reflection
 
+open Serilog.Core
+open Serilog.Events
+open Serilog.Sinks.ApplicationInsights.TelemetryConverters
+
+open WebApplication.Domain.Extensions
+
+type ApplicationVersion() =
+    static member Version =
+        Assembly
+            .GetAssembly(typeof<ApplicationVersion>)
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+            .InformationalVersion
+
 type CloudRoleNameInitializer() =
-    
+
     interface ITelemetryInitializer with
-        member this.Initialize(telemetry: ApplicationInsights.Channel.ITelemetry) = 
+        member this.Initialize(telemetry: ApplicationInsights.Channel.ITelemetry) =
             telemetry.Context.Cloud.RoleName <- "htmx-poc"
 
 type ComponentVersionInitializer() =
 
     interface ITelemetryInitializer with
         member this.Initialize(telemetry: ApplicationInsights.Channel.ITelemetry) =
-            telemetry.Context.Component.Version <-
-                Assembly
-                    .GetAssembly(
-                        typeof<ComponentVersionInitializer>
-                    )
-                    .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
-                    .InformationalVersion
+            telemetry.Context.Component.Version <- ApplicationVersion.Version
 
 type AuthenticatedUserInitializer(httpContextAccessor: IHttpContextAccessor) =
 
     interface ITelemetryInitializer with
         member this.Initialize(telemetry: ApplicationInsights.Channel.ITelemetry) =
-            if httpContextAccessor.HttpContext <> null
+            if
+                httpContextAccessor.HttpContext <> null
                 && httpContextAccessor.HttpContext.User <> null
-                && httpContextAccessor.HttpContext.User.Identity.IsAuthenticated then
+                && httpContextAccessor.HttpContext.User.Identity.IsAuthenticated
+            then
                 telemetry.Context.User.AuthenticatedUserId <- httpContextAccessor.HttpContext.User.Identity.Name
+
+type OperationIdEnricher() =
+
+    interface ILogEventEnricher with
+        member this.Enrich(logEvent, propertyFactory) =
+            let currentActivity = Activity.Current
+
+            if currentActivity <> null then
+                logEvent.AddPropertyIfAbsent(
+                    new LogEventProperty("Operation Id", new ScalarValue(currentActivity.TraceId))
+                )
+
+                logEvent.AddPropertyIfAbsent(new LogEventProperty("Parent Id", new ScalarValue(currentActivity.SpanId)))
+
+type OperationTelemetryConverter() =
+    inherit TraceTelemetryConverter()
+
+    let tryGetScalarProperty (logEvent: LogEvent) propertyName =
+        let hasScalarValue, value = logEvent.Properties.TryGetValue(propertyName)
+
+        if hasScalarValue && value :? ScalarValue then
+            true, Some((value :?> ScalarValue).ToString())
+        else
+            false, None
+
+    override _.Convert(logEvent, formatProvider) =
+
+        ``base``.Convert(logEvent, formatProvider)
+        |> Seq.map (fun telemetry ->
+            let hasOperationId, operationId = tryGetScalarProperty logEvent "Operation Id"
+
+            let hasParentOperationId, parentOperationId =
+                tryGetScalarProperty logEvent "Parent Id"
+
+            if hasOperationId then
+                telemetry.Context.Operation.Id <- operationId |> Option.defaultValue String.defaultValue
+
+            if hasParentOperationId then
+                telemetry.Context.Operation.ParentId <- parentOperationId |> Option.defaultValue String.defaultValue
+
+            telemetry)
--- WebApplication/Program.fs
+++ WebApplication/Program.fs
@@ -2,10 +2,15 @@ namespace WebApplication
 
 #nowarn "20"
 
+open System
+
 open Microsoft.ApplicationInsights.Extensibility
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
+
+open Serilog
+open Serilog.Events
 
 open WebApplication.Infrastructure.Database
 open WebApplication.Infrastructure.UserDatabase
@@ -13,40 +18,68 @@ open WebApplication.Infrastructure.HtmlTemplate
 open WebApplication.Infrastructure.Telemetry
 
 module Program =
+    
+    [<Literal>]
+    let SuccessExitCode = 0
 
-    let exitCode = 0
+    [<Literal>]
+    let FailureExitCode = -1
 
     [<EntryPoint>]
-    let main args =
-        let builder = WebApplication.CreateBuilder(args)
+    let main args =        
+        try
+            try
+                let builder = WebApplication.CreateBuilder(args)
 
-        builder.Services.AddSqlDatabase()
-        builder.Services.AddUserDatabase()
-        builder.Services.AddHtmlTemplate()
-        builder.Services.AddControllers()
+                builder.Services.AddSqlDatabase()
+                builder.Services.AddUserDatabase()
+                builder.Services.AddHtmlTemplate()
+                builder.Services.AddControllers()
 
-        builder
-            .Services
-            .AddApplicationInsightsTelemetry()
-            .AddSingleton<ITelemetryInitializer, CloudRoleNameInitializer>()
-            .AddSingleton<ITelemetryInitializer, ComponentVersionInitializer>()
-            .AddSingleton<ITelemetryInitializer, AuthenticatedUserInitializer>()
+                builder
+                    .Services
+                    .AddApplicationInsightsTelemetry()
+                    .AddSingleton<ITelemetryInitializer, CloudRoleNameInitializer>()
+                    .AddSingleton<ITelemetryInitializer, ComponentVersionInitializer>()
+                    .AddSingleton<ITelemetryInitializer, AuthenticatedUserInitializer>()
         
-        let app = builder.Build()
+                builder.Host.UseSerilog(Action<HostBuilderContext,IServiceProvider,LoggerConfiguration>(
+                    fun context provider loggerConfig ->
+                        loggerConfig
+                            .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                            .WriteTo.Console()     
+                            .WriteTo.ApplicationInsights(provider.GetRequiredService<TelemetryConfiguration>(), OperationTelemetryConverter())
+                            .Enrich.With<OperationIdEnricher>()
+                            .Enrich.WithMachineName()
+                            .Enrich.FromLogContext()
+                            .Enrich.WithProperty("Version", ApplicationVersion.Version)
+                            .Enrich.WithProperty("Application", "HTMX-POC")
+                        ()
+                ))
 
-        if builder.Environment.IsDevelopment() then
-            app.UseDeveloperExceptionPage()
-        else
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts()
+                let app = builder.Build()
 
-        app.UseHttpsRedirection()
-        app.UseStaticFiles()
-        app.UseRouting()
-        app.UseAuthorization()
+                if builder.Environment.IsDevelopment() then
+                    app.UseDeveloperExceptionPage()
+                else
+                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                    app.UseHsts()
 
-        app.MapControllerRoute(name = "default", pattern = "{controller=Home}/{action=Index}/{id?}")
+                app.UseSerilogRequestLogging()
 
-        app.Run()
+                app.UseHttpsRedirection()
+                app.UseStaticFiles()
+                app.UseRouting()
+                app.UseAuthorization()
 
-        exitCode
+                app.MapControllerRoute(name = "default", pattern = "{controller=Home}/{action=Index}/{id?}")
+
+                app.Run()
+
+                SuccessExitCode
+            with ex -> 
+                Console.Error.WriteLine(ex)
+            
+                FailureExitCode
+        finally            
+            Log.CloseAndFlush()
